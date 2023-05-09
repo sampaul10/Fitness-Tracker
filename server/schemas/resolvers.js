18ce8f314547aac78af7929295f7b4ae8874b029
Taeyoung Park <taeyoung.park@uconn.edu>
2023-05-09 09:25:28 -0400

    added mutations and queries to front-end

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,18 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    /*
+        me: async (parent, args, context) => {
+      if(context.user){
+        const userData = await User.findOne({ _id: context.user._id })
+          .populate('achievements')
+          .populate('workouts');
+
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    */
     users: async () => {
       return User.find()
       .populate("achievements")
