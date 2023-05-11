2f7e9c9d431a8bf8dfd9d16ed75090dda6b91faa
Samson Paul <samson.paul@hotmail.com>
2023-05-10 21:02:33 -0400

    added new model

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Workout = require('./Workout');
 const Achievement = require('./Achievement');
+const Exercise = require('./Exercise');
 
 
-module.exports = { User, Workout, Achievement };
+module.exports = { User, Workout, Achievement, Exercise };
