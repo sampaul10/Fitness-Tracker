37e2cad330a25d31abfca39c0e363c3dd4e2f666
jrwesch <jrwesch@gmail.com>
2023-05-11 21:30:10 -0400

    mypage css, categorymenu css

--- client/src/pages/MyPage/MyPage.js
+++ client/src/pages/MyPage/MyPage.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import profilePic from "../assets/images/profile-pic.png";
-import CategoryMenu from "../components/CategoryMenu";
-import WorkoutList from "../components/WorkoutList";
-import AddWorkoutForm from '../components/AddWorkoutForm';
+import profilePic from "../../assets/images/profile-pic.png";
+import CategoryMenu from "../../components/CategoryMenu";
+import WorkoutList from "../../components/WorkoutList";
+import AddWorkoutForm from '../../components/AddWorkoutForm';
 import { Modal, Button } from "react-bootstrap";
+import './MyPage.css';
 
 const MyPage = () => {
     const [showModal, setShowModal] = useState(false);
@@ -15,7 +16,7 @@ const MyPage = () => {
         <div>
             <h2 className="section-header">My Page</h2>
             <img src={profilePic} alt="Profile Pic" />
-            <div>
+            <div className="add-workout">
                 <CategoryMenu /> {/* Showing exercise category for user's excersie list (categorized by target) */}
                 {/*  Showing exercise user's exercise list and option to remove (fetch user's data's 'workouts' and loop thru to display it along with the delete button(or share button)*/}
                 <WorkoutList />
@@ -32,7 +33,7 @@ const MyPage = () => {
                         <Button onClick={closeAddWorkoutModal}>Close</Button>
                     </Modal.Footer>
                 </Modal>
-                <Button onClick={showAddWorkoutModal}>Add Workout</Button>
+                <Button className="show-add-workout-modal" onClick={showAddWorkoutModal}>Add Workout</Button>
             </div>
 
         </div>
