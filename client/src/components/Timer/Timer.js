f2fbb9cbae772f5caaab490f729b32bea78eb0f8
Samson Paul <samson.paul@hotmail.com>
2023-05-12 19:37:30 -0400

    initial commit

--- client/src/components/Timer/Timer.js
+++ client/src/components/Timer/Timer.js
@@ -33,6 +33,19 @@ const Timer = () => {
       </svg>
     </div>
     </div>
+
+    <div>
+    <div className="countdown">
+      <svg viewBox="-50 -50 100 100" strokeWidth="10">
+        <circle r="45"></circle>
+        <circle
+          r="45"
+          strokeDasharray="282.7433388230814"
+          strokeDashoffset="282.7433388230814px"
+        ></circle>
+      </svg>
+    </div>
+    </div>
         </span>
       );
     }
