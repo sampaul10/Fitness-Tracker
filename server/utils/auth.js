6079084aeba341f6d69da01c2182808d6d1a4b29
Taeyoung Park <taeyoung.park@uconn.edu>
2023-05-13 18:12:53 -0400

    fixed few files for heroku deployment

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
+const dotenv = require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+const secret = process.env.secret;
 const expiration = '2h';
 
 module.exports = {
