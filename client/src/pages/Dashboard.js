e67d753c8882fba1aeebcfe6fd8f50089385cf52
Taeyoung Park <taeyoung.park@uconn.edu>
2023-05-12 21:50:03 -0400

    Added more css styling in general

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,18 +1,25 @@
-
 import React, { useState } from "react";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import './Dashboard.css';
 
 const Dashboard = () => {
 
     return (
-        <div className="dashpic">
-            <header className="header">
-                <h1>U-Fit</h1>
-            </header>
-            <h1>Dashboard Page!</h1> 
-            
+        <div>
+            <div className="dashpic">
+                <header className="header">
+                    <h1 className="u-fit">Welcome to U-Fit!</h1>
+                </header>
+                <h2>Dashboard Page</h2>
+
+            </div>
+            <div>
+                <p>hello world!</p>
+            </div>
         </div>
+
+
     );
 }
 
