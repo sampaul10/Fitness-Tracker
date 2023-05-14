1bba9426303b38e6389a6180ece69a7627d2e9ea
Taeyoung Park <taeyoung.park@uconn.edu>
2023-05-14 16:37:13 -0400

    modified images and heroku deployment test

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import './Dashboard.css';
-import image1 from '../assets/images/dashboard/image1.jpeg'
-import image2 from '../assets/images/dashboard/image2.jpeg'
-import image3 from '../assets/images/dashboard/image3.jpeg'
-import image4 from '../assets/images/dashboard/image4.jpeg'
-import image5 from '../assets/images/dashboard/image5.jpeg'
+import image1 from '../assets/images/dashboard/image1.jpg'
+import image2 from '../assets/images/dashboard/image2.jpg'
+import image3 from '../assets/images/dashboard/image3.jpg'
+import image4 from '../assets/images/dashboard/image4.jpg'
+import image5 from '../assets/images/dashboard/image5.jpg'
 
 const Dashboard = () => {
     const images = [
